52dde4f138ba1021d9e43f8e7c1283e9fd1be9c3
Beliz Yilmaz <belizbel@live.unc.edu>
2022-02-08 09:52:39 -0500

    Rush page edited

--- src/pages/Rush.js
+++ src/pages/Rush.js
@@ -9,11 +9,9 @@ export default function Rush() {
             <Header />
             <div className="rush">
                 <h1>R<span>us</span>h</h1>
-                <h2 class="rush-sub">Interested in rushing KTP this fall? Follow these easy steps:</h2>
+                <h2 class="rush-sub">Interested in rushing KTP?</h2>
                 <br/>
-                <h2>1. Fill out our <strong><a class="fb-group-link" target="_blank" href='https://forms.gle/rj89gU4B2DbgG8iM6'>Interest Form</a></strong></h2>
-                <h2>2. Join our <strong><a class="fb-group-link" target="_blank" href='https://www.facebook.com/groups/838024823748330/'>Fall 2021 Rush Group</a></strong> on Facebook!
-                </h2>
+                <h2>Stay up to date with us for more information about Fall 2022 Rush!</h2>
             </div>
             <Footer />            
         </div>
